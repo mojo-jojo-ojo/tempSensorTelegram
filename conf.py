6b2b2e0abf2dcbc2cd834678338b7169069b329c
Ojaswini Nimbalkar <44779048+mojo-jojo-ojo@users.noreply.github.com>
2020-05-31 20:10:10 +0530

    Add files via upload

---
+++ conf.py
@@ -0,0 +1,7 @@
+"""Configurations for telegram_alert.py"""
+bolt_api_key = ""                 # This is your Bolt Cloud API Key
+device_id = ""                    # This is the device ID and will be similar to BOLTXXXX where XXXX is some numbers
+telegram_chat_id = "@XXXX"            # This is the channel ID of the created Telegram channel. Paste after @ symbol.
+telegram_bot_id = "botXXXX"           # This is the bot token no. of the created Telegram Bot. Paste after bot text.
+threshold = 250                       # Threshold beyond which the alert should be sent
+
